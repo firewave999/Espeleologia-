dbab97c9101abb2d02ad6cfff7cecd5d3dee5c83
firewave999 <wles.9909@gmail.com>
2026-05-22 09:52:35 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bioespeleo-v2";
+const CACHE = "bioespeleo-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./xlsx.full.min.js"];
 
 self.addEventListener("install", e => {
